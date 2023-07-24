504d5ea53c1fdf8c2e42eb1bd7861e51bdc6e0df
sugarmanbrandon <me@brandonsugarman.me>
2023-07-23 21:36:51 -0700

    test

--- ampcx.webflow/clientportal/main.js
+++ ampcx.webflow/clientportal/main.js
@@ -54,7 +54,7 @@ const firebaseConfig = {
           // Signed in
           var user = userCredential.user;
           // Redirect to user page
-          window.location.href = "https://www.ampcx.com/ampcx.webflow/clientportal/user.html";
+          window.location.href = "https://www.ampcx.com/ampcx.webflow/clientportal/user.html";}
       .catch((error) => {
           var errorCode = error.code;
           var errorMessage = error.message;
